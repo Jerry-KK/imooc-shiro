772e387fd7017f5b8030e7169093f039d0846ef1
lethekk <15868308739@163.com>
2021-07-28 23:56:32 +0800

    # 新增 shiro-spring 配置类

---
+++ shiro-web/src/test/java/cn/lethekk/shiroweb/ShiroWebApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lethekk.shiroweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiroWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
